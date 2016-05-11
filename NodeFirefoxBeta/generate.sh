2a82bcacd99650f4c80da3fadba023a241041928
patxi <patxi.gortazar@gmail.com>
2016-05-11 11:25:38 +0200

    [Release 2.53.0] Fix FF beta
    Change-Id: I7f01346821879be59d7af68981cf482464d1d577

--- NodeFirefoxBeta/generate.sh
+++ NodeFirefoxBeta/generate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 VERSION=$1
 
-echo FROM selenium/node-base:$VERSION > ./Dockerfile
-cat ../NodeFirefox/Dockerfile.txt >> ./Dockerfile
-sed -i 's|RUN apt-get update -qqy|RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0AB215679C571D1C8325275B9BDB3D89CE49EC21 \\ \n \&\& echo "deb http://ppa.launchpad.net/mozillateam/firefox-next/ubuntu trusty main" >> /etc/apt/sources.list.d/firefox-beta.list \\ \n \&\& apt-get update -qqy \ |' ./Dockerfile
-cp ../NodeChrome/config.json .
+echo FROM $KURENTO_REGISTRY_URI/kurento/node-firefox:$VERSION > ./Dockerfile
+cat ./Dockerfile.txt >> ./Dockerfile
