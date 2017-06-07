67d1fa8b85c1c8fee1f14e3e965a36726bd44ade
Fede Diaz <nordri@gmail.com>
2017-06-07 16:27:38 +0200

    [chrome-dev] Upgrade driver

--- NodeChromeDev/generate.sh
+++ NodeChromeDev/generate.sh
@@ -3,10 +3,11 @@ VERSION=$1
 
 echo FROM kurento/selenium-node-base:$VERSION > ./Dockerfile
 cat ../NodeChrome/Dockerfile.txt >> ./Dockerfile
+sed -i 's/CHROME_DRIVER_VERSION 2.21/CHROME_DRIVER_VERSION 2.25/' ./Dockerfile
 sed -i 's/google-chrome-stable/google-chrome-unstable/' ./Dockerfile
 sed -i 's|COPY chrome_launcher.sh /opt/google/chrome/google-chrome|COPY chrome_launcher.sh /opt/google/chrome-beta/google-chrome|' ./Dockerfile
 sed -i 's|RUN chmod +x /opt/google/chrome/google-chrome|RUN chmod +x /opt/google/chrome-beta/google-chrome|' ./Dockerfile
-sed -i 's|https://chromedriver.storage.googleapis.com/$CHROME_DRIVER_VERSION/chromedriver_linux64.zip|http://chromedriver-data.storage.googleapis.com/continuous/chromedriver_linux64_2.23.412753.zip|' ./Dockerfile
 cp ../NodeChrome/config.json .
 cp ../NodeChrome/chrome_launcher.sh .
 sed -i 's|export CHROME_VERSION_EXTRA="stable"|export CHROME_VERSION_EXTRA="unstable"|' ./chrome_launcher.sh
+
