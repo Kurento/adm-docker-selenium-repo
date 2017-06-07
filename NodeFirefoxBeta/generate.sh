ccbc59bc2f5d4413e3a6a50f263805484defa53b
Fede Diaz <nordri@gmail.com>
2017-06-07 15:14:10 +0200

    Public repo issue

--- NodeFirefoxBeta/generate.sh
+++ NodeFirefoxBeta/generate.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 VERSION=$1
 
-echo FROM $KURENTO_REGISTRY_URI/kurento/node-firefox:$VERSION > ./Dockerfile
+echo FROM kurento/node-firefox:$VERSION > ./Dockerfile
 cat ./Dockerfile.txt >> ./Dockerfile
